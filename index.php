ddf4ec82d42ecec9df244ab7a4d7ae7b5bef2065
Frédéric NOËL <contact@fredericnoel.com>
2022-07-11 16:16:44 +0200

    Exemple affichage n-ième caractère d'une chaîne

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-$version = 8;
-$langue = "fr";
-$masque = "PHP %d est disponible en %s";
-printf($masque, $version, $langue);
-
-/* Masques de printf()
-* %s chaîne de caractères
-* %d nombre entier signé
-* %u nombre entier non signé
-* %f nombre à virgule flottante
-* %b binaire
-* %o octal
-* %x hexadécimal
-* %X hexadécimal avec majuscules
-* %% affiche %
-*/
+$chaine = "Ceci est une phrase.";
+echo $chaine[1];
+// Avant PHP8, on pouvait également écrire $chaine{1}
